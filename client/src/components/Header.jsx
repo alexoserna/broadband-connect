342e5f25b38620eb4d331f0377b7ec8700dc9905
alexoserna <118146045+alexoserna@users.noreply.github.com>
2025-01-12 21:47:21 -0500

    added broadband connect logo. Fixed mui installation by installing emotion/cache

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,8 +1,32 @@
 import { Link } from "react-router-dom";
 import { useState } from "react";
+import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+
 
 const Header = () => {
+    <AppBar position="static" color="primary">
+    <Toolbar>
+      {/* Left-aligned icon */}
+      <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+        <MenuIcon />
+      </IconButton>
 
+      {/* Centered or left-aligned title */}
+      <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+        Broadband Connect
+      </Typography>
+
+      {/* Right-aligned buttons */}
+      <Button color="inherit">Login</Button>
+      <Button color="inherit">Sign Up</Button>
+    </Toolbar>
+  </AppBar>
 };
 
     export default Header;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,30 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+// Create a custom theme
+const theme = createTheme({
+
+  palette:{
+    primary:{
+      main: '#C0392B',
+    },
+    secondary:{
+      main: '#007B43',
+    },
+  }
+
+});
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ThemeProvider theme = {theme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
   </StrictMode>,
 )
