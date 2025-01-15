415de5b768ae9a186907bcbd4ad4693dd20cdf16
alexoserna <118146045+alexoserna@users.noreply.github.com>
2025-01-14 20:28:00 -0500

    added a themeDark for future implementation. Will currently leave theme alone until I implemenet themeDark then it will be themeLight. Finished adding all colors belonging to the color palette.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,6 +5,7 @@ import './index.css';
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { GlobalStyles } from '@mui/material';
 
 // Create a custom theme
 const theme = createTheme({
@@ -12,20 +13,44 @@ const theme = createTheme({
   palette:{
     primary: {
       main: '#C0392B',
-      dark: '#D44F40'
     },
     secondary:{
       main: '#007B43',
+    },
+    accent: {
+      main: '#D4A017',
     },
     // background colors
     backgroundCream: {
       main: '#F4ECE4',
       card: '#F1E3DA',
-      dark: '#1A130A',
     },
     textColor:{
       main: '#2C1F0E',
-      dark: 'F2E5D4',
+    },
+  },
+
+});
+
+// Create dark mode theme to be implemented later
+const themeDark = createTheme({
+
+  palette:{
+    primary: {
+      main: '#D44F40',
+    },
+    secondary:{
+      main: '#85FFC8',
+    },
+    accent:{
+      main:'#E8B22C'
+    },
+    // background colors
+    backgroundCream: {
+      main: '#1A130A',
+    },
+    textColor:{
+      main: 'F2E5D4',
     },
   },
 
@@ -35,6 +60,11 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemeProvider theme = {theme}>
       <CssBaseline />
+      <GlobalStyles 
+        styles={{
+          body: {backgroundColor: theme.palette.backgroundCream.main},
+        }}
+      />
       <BrowserRouter>
         <App />
       </BrowserRouter>
